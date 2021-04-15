87aa98f43e62c8f63ca4a23d2c40d12aa490a9bf
Eduardo Asafe <e@asafe.dev>
2021-04-15 15:48:37 +0800

    Slightly better code organization

--- test/Main.purs
+++ test/Main.purs
@@ -35,82 +35,82 @@ main = TUM.runTest do
       TU.suite "select" do
             TU.suite "single column" do
                   TU.test "scalar" do
-                        let query = print $ select 3
+                        let query = toQuery $ select 3
                         noParameters "SELECT 3" query
                   TU.test "field" do
-                        let query = print $ select id
+                        let query = toQuery $ select id
                         noParameters "SELECT id" query
                   TU.test "table" do
-                        let query = print $ select users
+                        let query = toQuery $ select users
                         noParameters "SELECT users.*" query
 
             TU.suite "columns" do
                   TU.test "scalars" do
-                        let query = print $ select (3 /\ 4 /\ 5)
+                        let query = toQuery $ select (3 /\ 4 /\ 5)
                         noParameters "SELECT 3, 4, 5" query
                   TU.test "fields" do
-                        let query = print $ select (id /\ name)
+                        let query = toQuery $ select (id /\ name)
                         noParameters "SELECT id, name" query
                   TU.test "mixed" do
-                        let query = print $ select (users /\ id /\ 5)
+                        let query = toQuery $ select (users /\ id /\ 5)
                         noParameters "SELECT users.*, id, 5" query
 
             TU.suite "sub query" do
                   TU.test "scalars" do
-                        let query = print $ select (3 /\ (select 34 # from users # wher (name .=. surname) {}))
+                        let query = toQuery $ select (3 /\ (select 34 # from users # wher (name .=. surname) {}))
                         noParameters "SELECT 3, (SELECT 34 FROM users WHERE name = surname)" query
                   TU.test "fields" do
-                        let query = print $ select (id /\ (select (Field :: Field "sent") # from messages))
+                        let query = toQuery $ select (id /\ (select (Field :: Field "sent") # from messages))
                         noParameters "SELECT id, (SELECT sent FROM messages)" query
                   TU.test "mixed" do
-                        let query = print $ select (3 /\ (select id # from messages) /\ id /\ name /\ users /\ (select (Field :: Field "sent") # from messages) /\ (select 5 # from messages))
+                        let query = toQuery $ select (3 /\ (select id # from messages) /\ id /\ name /\ users /\ (select (Field :: Field "sent") # from messages) /\ (select 5 # from messages))
                         noParameters "SELECT 3, (SELECT id FROM messages), id, name, users.*, (SELECT sent FROM messages), (SELECT 5 FROM messages)" query
 
       TU.suite "from" do
             TU.test "table" do
-                  let query = print $ select 34 # from messages
+                  let query = toQuery $ select 34 # from messages
                   noParameters "SELECT 34 FROM messages" query
 
       TU.suite "where" do
             TU.suite "field" do
                   TU.test "equals" do
-                        let query = print $ select 34 # from users # wher (name .=. surname) {}
+                        let query = toQuery $ select 34 # from users # wher (name .=. surname) {}
                         withParameters "SELECT 34 FROM users WHERE name = surname" query
                   TU.test "not equals" do
-                        let query = print $ select 34 # from users # wher (name .<>. surname) {}
+                        let query = toQuery $ select 34 # from users # wher (name .<>. surname) {}
                         withParameters "SELECT 34 FROM users WHERE name <> surname" query
 
             TU.suite "parameters" do
                   TU.test "equals" do
-                        let query = print $ select id # from users # wher (name .=. (Parameter :: Parameter "name")) {name : "josh"}
+                        let query = toQuery $ select id # from users # wher (name .=. (Parameter :: Parameter "name")) {name : "josh"}
                         withParameters "SELECT id FROM users WHERE name = @name" query
                   TU.test "not equals" do
-                        let query = print $ select id # from users # wher (id .<>. (Parameter :: Parameter "id")) {id: 3}
+                        let query = toQuery $ select id # from users # wher (id .<>. (Parameter :: Parameter "id")) {id: 3}
                         withParameters "SELECT id FROM users WHERE id <> @id" query
 
             TU.suite "logical operands" do
                   TU.suite "and" do
                         TU.test "single" do
-                              let query = print $ select id # from users # wher (name .=. (Parameter :: Parameter "name") .&&. name .=. surname) {name : "josh"}
+                              let query = toQuery $ select id # from users # wher (name .=. (Parameter :: Parameter "name") .&&. name .=. surname) {name : "josh"}
                               withParameters "SELECT id FROM users WHERE (name = @name AND name = surname)" query
                         TU.test "many" do
-                              let query = print $ select id # from users # wher (name .=. (Parameter :: Parameter "name") .&&. name .=. surname .&&. surname .<>. (Parameter :: Parameter "surname")) {name : "josh", surname: "j."}
+                              let query = toQuery $ select id # from users # wher (name .=. (Parameter :: Parameter "name") .&&. name .=. surname .&&. surname .<>. (Parameter :: Parameter "surname")) {name : "josh", surname: "j."}
                               withParameters "SELECT id FROM users WHERE ((name = @name AND name = surname) AND surname <> @surname)" query
 
                   TU.suite "or" do
                         TU.test "single" do
-                              let query = print $ select id # from users # wher (name .=. (Parameter :: Parameter "name") .||. name .=. surname) {name : "josh"}
+                              let query = toQuery $ select id # from users # wher (name .=. (Parameter :: Parameter "name") .||. name .=. surname) {name : "josh"}
                               withParameters "SELECT id FROM users WHERE (name = @name OR name = surname)" query
                         TU.test "many" do
-                              let query = print $ select id # from users # wher (name .=. (Parameter :: Parameter "name") .||. name .=. surname .||. surname .<>. (Parameter :: Parameter "surname")) {name : "josh", surname: "j."}
+                              let query = toQuery $ select id # from users # wher (name .=. (Parameter :: Parameter "name") .||. name .=. surname .||. surname .<>. (Parameter :: Parameter "surname")) {name : "josh", surname: "j."}
                               withParameters "SELECT id FROM users WHERE ((name = @name OR name = surname) OR surname <> @surname)" query
 
                   TU.suite "mixed" do
                         TU.test "not bracketed" do
-                              let query = print $ select id # from users # wher (id .=. (Parameter :: Parameter "id3") .||. id .=. (Parameter :: Parameter "id33") .&&. id .=. (Parameter :: Parameter "id333")) {id3 : 3, id33: 33, id333 : 333}
+                              let query = toQuery $ select id # from users # wher (id .=. (Parameter :: Parameter "id3") .||. id .=. (Parameter :: Parameter "id33") .&&. id .=. (Parameter :: Parameter "id333")) {id3 : 3, id33: 33, id333 : 333}
                               withParameters "SELECT id FROM users WHERE (id = @id3 OR (id = @id33 AND id = @id333))" query
                         TU.test "bracketed" do
-                              let query = print $ select id # from users # wher ((id .=. (Parameter :: Parameter "id3") .||. id .=. (Parameter :: Parameter "id33")) .&&. id .=. (Parameter :: Parameter "id333")) {id3 : 3, id33: 33, id333 : 333}
+                              let query = toQuery $ select id # from users # wher ((id .=. (Parameter :: Parameter "id3") .||. id .=. (Parameter :: Parameter "id33")) .&&. id .=. (Parameter :: Parameter "id333")) {id3 : 3, id33: 33, id333 : 333}
                               withParameters "SELECT id FROM users WHERE ((id = @id3 OR id = @id33) AND id = @id333)" query
 
 noParameters :: forall p. String -> Query p -> _
