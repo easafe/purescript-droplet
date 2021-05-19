3eb11b70abb2b023771b7f6ce96e2a1229b461e4
Eduardo Asafe <e@asafe.dev>
2021-05-19 13:33:20 +0800

    Tests for unsafe queries

--- test/Main.purs
+++ test/Main.purs
@@ -7,6 +7,7 @@ import Test.Delete as TD
 import Test.Insert as TI
 import Test.Select as TS
 import Test.Unit.Main as TUM
+import Test.Unsafe as TUS
 import Test.Update as TU
 
 main :: Effect Unit
@@ -15,3 +16,4 @@ main = TUM.runTest do
       TI.tests
       TU.tests
       TD.tests
+      TUS.tests
--- test/Unsafe.purs
+++ test/Unsafe.purs
@@ -1,19 +1,24 @@
 module Test.Unsafe where
 
 import Droplet.Internal.Edsl.Language
-import Droplet.Internal.Edsl.Condition
 import Prelude
 import Test.Types
 
+import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested ((/\))
+import Droplet.Internal.Mapper.Query (Query(..))
 import Droplet.Internal.Mapper.Query as Query
 import Test.Model as TM
 import Test.Unit (TestSuite)
 import Test.Unit as TU
+import Test.Unit.Assert as TUA
 
 tests :: TestSuite
 tests = do
-      TU.test "update" do
-            let q = update users # set (surname /\ "Sue")
-            TM.parameterized "UPDATE users SET surname = $1" $ Query.query q
-            TM.result q []
+      TU.suite "unsafe queries" do
+            TU.test "select" do
+                  let (plan /\ q /\ parameters) = Nothing /\ "UPDATE users SET name = @name WHERE id = @id" /\ { name: "Suzy", id: 23 }
+                  let Query _ dollaredQ _ = Query.unsafeQuery plan q parameters
+                  --parameters are replaced by field order
+                  TUA.equal "UPDATE users SET name = $2 WHERE id = $1" dollaredQ
+                 -- TM.unsafeResult plan q parameters
