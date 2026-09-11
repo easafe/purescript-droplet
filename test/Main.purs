59866a89386e8b36270d3526229cb5721303cb87
Eduardo Asafe <e@asafe.dev>
2026-09-11 20:42:20 +0200

    Update spago and test format

--- test/Main.purs
+++ test/Main.purs
@@ -3,35 +3,34 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Aff as EA
+import Test.Alter as TAL
 import Test.As as TA
 import Test.Create as TC
 import Test.Delete as TD
 import Test.Distinct as TDS
+import Test.Drop as TDR
 import Test.From as TF
 import Test.Function as TFC
 import Test.GroupBy as TG
 import Test.Insert as TI
 import Test.Join as TJ
 import Test.Limit as TL
-import Test.NakedSelect as TNS
 import Test.Migration as TMG
+import Test.NakedSelect as TNS
 import Test.Offset as TOF
 import Test.OrderBy as TO
 import Test.Returning as TR
-import Test.Spec.Runner as TSR
-import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Reporter.Tap (tapReporter)
+import Test.Spec.Runner.Node as TSR
 import Test.SubQuery as TSQ
 import Test.Transaction as TST
 import Test.Union as TUN
 import Test.Unsafe as TUS
 import Test.Update as TU
-import Test.Drop as TDR
-import Test.Alter as TAL
 import Test.Where as TW
 
 main ∷ Effect Unit
-main = EA.launchAff_ $ TSR.runSpec [ consoleReporter ] do
+main = TSR.runSpecAndExitProcess [ tapReporter ] do
       TUS.tests
       TI.tests
       TU.tests
